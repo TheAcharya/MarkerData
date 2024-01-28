4e6aeccf7856d685a2e395c5481fbd62b5249045
Várady Milán <61704770+MilanVarady@users.noreply.github.com>
2024-01-28 16:08:01 +0100

    Revert "Remove ProExtensionHost from Workflow Extension"
    This reverts commit ff9e6f24e9df1acbe231b9c2eff11f9826187d80.

--- Source/Marker Data/Workflow Extension/WorkflowExtensionViewController.swift
+++ Source/Marker Data/Workflow Extension/WorkflowExtensionViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import Cocoa
+import ProExtensionHost
 import SwiftUI
 
 @objc class WorkflowExtensionViewController: NSViewController {
@@ -18,5 +19,10 @@ import SwiftUI
         self.view.addSubview(swiftuiView)
         swiftuiView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
         swiftuiView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+    }
+	
+    @objc var hostInfoString: String {
+        let host = ProExtensionHostSingleton() as! FCPXHost
+        return String(format:"%@ %@", host.name, host.versionString)
     }
 }
