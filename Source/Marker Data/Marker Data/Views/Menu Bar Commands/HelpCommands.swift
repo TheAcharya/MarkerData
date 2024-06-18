260fad5d3caf066042b55ea9b773c115787da3eb
Vigneswaran Rajkumar <118706051+IAmVigneswaran@users.noreply.github.com>
2024-06-18 08:45:02 +0800

    Update HelpCommands.swift

--- Source/Marker Data/Marker Data/Views/Menu Bar Commands/HelpCommands.swift
+++ Source/Marker Data/Marker Data/Views/Menu Bar Commands/HelpCommands.swift
@@ -12,20 +12,21 @@ struct HelpCommands: Commands {
     var body: some Commands {
         //Add Help And Debug Menu Buttons
         CommandGroup(replacing: .help) {
-            Link("Website", destination: URL(string: "https://markerdata.theacharya.co")!)
+            Link("Marker Data Website", destination: URL(string: "https://markerdata.theacharya.co")!)
             Link("Keyboard Shortcuts", destination: URL(string: "https://markerdata.theacharya.co/user-guide/keyboard-shortcuts/")!)
-            Link("Source Code", destination: URL(string: "https://github.com/TheAcharya/MarkerData")!)
-            
-            Divider()
-            
-            Link("Release Notes", destination: URL(string: "https://markerdata.theacharya.co/release-notes/")!)
-            Link("Sponsor Marker Data", destination: URL(string: "https://github.com/sponsors/TheAcharya")!)
-            Link("About Marker Data", destination: URL(string: "https://markerdata.theacharya.co/credits/")!)
+            Link("Troubleshooting", destination: URL(string: "https://markerdata.theacharya.co/troubleshooting/")!)
             
             Divider()
             
             Link("Send Feedback", destination: URL(string: "https://github.com/TheAcharya/MarkerData/issues")!)
             Link("Discussions", destination: URL(string: "https://github.com/TheAcharya/MarkerData/discussions")!)
+
+            Divider()
+            
+            Link("Release Notes", destination: URL(string: "https://markerdata.theacharya.co/release-notes/")!)
+            Link("Source Code", destination: URL(string: "https://github.com/TheAcharya/MarkerData")!)
+            Link("Sponsor Marker Data", destination: URL(string: "https://github.com/sponsors/TheAcharya")!)
+            Link("About Marker Data", destination: URL(string: "https://markerdata.theacharya.co/credits/")!)
             
             Divider()
             
