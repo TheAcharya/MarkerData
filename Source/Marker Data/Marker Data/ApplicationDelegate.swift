32a7cb6db658d150588c1b4f43acccf732ba5f35
Várady Milán <61704770+MilanVarady@users.noreply.github.com>
2024-06-30 18:47:27 +0200

    Fix update available badge

--- Source/Marker Data/Marker Data/ApplicationDelegate.swift
+++ Source/Marker Data/Marker Data/ApplicationDelegate.swift
@@ -14,14 +14,32 @@ class ApplicationDelegate: NSObject, NSApplicationDelegate, SPUUpdaterDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         updaterController = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: self, userDriverDelegate: nil)
+        updaterController.updater.checkForUpdatesInBackground()
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
 
-    @objc func updater(_ updater: SPUUpdater, didFindValidUpdate item: SUAppcastItem) {
-        // Notify SwiftUI view about the update
-        NotificationCenter.default.post(name: .updateAvailable, object: nil)
+    // Notify SwiftUI view if update is available
+    func bestValidUpdate(in appcast: SUAppcast, for updater: SPUUpdater) -> SUAppcastItem? {
+        // Compare current build number with the best available one
+        if let buildNumberString = appcast.items.first?.versionString,
+           let bestAvaiableBuildNumber = Int(buildNumberString),
+           let currentBuildNumber = Int(Bundle.main.buildNumber) {
+
+            if bestAvaiableBuildNumber > currentBuildNumber {
+                // Notify SwiftUI view about the update
+                NotificationCenter.default.post(name: .updateAvailable, object: nil)
+            }
+        }
+
+        return SUAppcastItem.empty()
     }
+
+    // For some reason this function is not called consistently so we use the bestValidUpdate instead
+//    func updater(_ updater: SPUUpdater, didFindValidUpdate item: SUAppcastItem) {
+//        // Notify SwiftUI view about the update
+//        NotificationCenter.default.post(name: .updateAvailable, object: nil)
+//    }
 }
